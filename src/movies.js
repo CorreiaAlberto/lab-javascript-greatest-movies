2da82e1a5f5ea54310a5c6e55c253be18f072569
CorreiaAlberto <alberto_cpv@hotmail.com>
2020-04-11 19:23:59 +0200

    Falta ultima iteracion

--- src/movies.js
+++ src/movies.js
@@ -35,23 +35,55 @@ function howManyMovies(peliculas) {
 
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
 function orderAlphabetically(peliculas) {
-    //copia array
-    let orderedMovies = Array.from(peliculas).sort(function (a, b) {
-        // comparador
-        if (a.title > b.title) return 1;
-        else return -1
-    })
-    return peliculas.splice(0, 20)
+    let orderedMovies = Array.from(peliculas);
+
+    function comparador(a, b) {
+        if (a.title < b.title) {
+            return 1
+        } else if (a.title > b.title) {
+            return -1
+        }
+        return 0
+    }
+    let final20 = orderedMovies.sort(comparador).slice(0, 20)
+    return final20
 }
-
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
-function ratesAverage() {
+function ratesAverage(peliculas) {
+    let averageRate = Array.from(peliculas)
+    if (peliculas.length === 0) {
+        return 0
+    }
 
+    function reduceRates(acc, pelicula) {
+        if (pelicula.rate) {
+            return acc + pelicula.rate;
+        } else {
+            return acc + 0;
+        }
+    }
+    return Math.round(averageRate.reduce(reduceRates, 0) / averageRate.length * 100) / 100;
 }
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
+function dramaMoviesRate(peliculas) {
+    let dramaRateArr = Array.from(peliculas)
+
+    function dramaRateFunc(dramaRateArr) {
+        if (dramaRateArr.genre.includes("Drama")) {
+            return true
+        }
+    }
+    dramaRateArr = dramaRateArr.filter(dramaRateFunc)
+    return ratesAverage(dramaRateArr)
+
+}
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(peliculas) {
 
+
+}
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
